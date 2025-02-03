a2c36efa7ff98cb8c9f2645c39f959633e01377f
Gui <guifty32@gmail.com>
2025-02-03 20:49:32 +0200

    Use State Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-function Square({value}) {
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
   function handleClick(){
-    console.log('Clicked!');
+    setValue('X!');
   }
   return <button className="square" onClick={handleClick}>{value}</button>
 
@@ -13,25 +16,25 @@ export default function Board() {
         {/* <button className="square">X</button>
         <button className="square">X</button>
         <button className="square">X</button> */}
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
         {/* <button className="square">X</button>
         <button className="square">X</button>
         <button className="square">X</button> */}
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
         {/* <button className="square">X</button>
         <button className="square">X</button>
         <button className="square">X</button> */}
-        <Square value="7"/>    
-        <Square value="8"/>    
-        <Square value="9"/>    
+        <Square />    
+        <Square />    
+        <Square />    
 
         </div>
     </>
